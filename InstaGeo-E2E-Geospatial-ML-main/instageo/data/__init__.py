0f7466eb3211b2a3dc5302d338e23a3201552163
humanzip <humain.zip@gmail.com>
2025-02-05 13:28:51 +0100

    Delete InstaGeo-E2E-Geospatial-ML-main directory
    /

--- InstaGeo-E2E-Geospatial-ML-main/instageo/data/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Data Handling for InstaGeo.
-
-This subpackage includes modules and utilities for loading, processing,
-and transforming geospatial data for analysis.
-"""
--- InstaGeo-E2E-Geospatial-ML-main/instageo/model/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Modeling Component for InstaGeo.
-
-This subpackage contains modules and classes for building, training, and evaluating
-geospatial models. It includes various algorithms and model architectures specialized
-for handling and analyzing geospatial data.
-"""
